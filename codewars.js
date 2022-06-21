1b1463f8496f8962b17ed3e8f2aea86977bc2352
Jose Sison <68295496+josesison1@users.noreply.github.com>
2022-06-21 11:47:58 -0700

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -290,3 +290,28 @@ function billboard(name, price = 30){
   }
   return sum
 } 
+
+
+
+
+// DESCRIPTION:
+// Remember the spongebob meme that is meant to make fun of people by repeating what they say in a mocking way?
+
+// You need to create a function that converts the input into this format, 
+// with the output being the same string expect there is a pattern of uppercase and lowercase letters.
+
+// Example:
+
+// input:  "stop Making spongebob Memes!"
+// output: "StOp mAkInG SpOnGeBoB MeMeS!"
+
+function spongeMeme(sentence) {
+  const newWord =  sentence.split("").map((v, i) => {
+    if (i % 2 !== 0) {
+      return v.toLowerCase()
+    } else {
+      return v.toUpperCase()
+    }
+  })
+  return newWord.join('')
+  }
