4023a14cf9314a7ba8a8cd4fa10c985d2cc8b192
Jose Sison <68295496+josesison1@users.noreply.github.com>
2022-07-05 21:29:01 -0700

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -395,3 +395,35 @@ var max = function(list){
     
   return Math.max(...list)
 }
+
+
+
+
+// DESCRIPTION:
+// You are given two sorted arrays that both only contain integers. 
+// Your task is to find a way to merge them into a single one, sorted in asc order. 
+// Complete the function mergeArrays(arr1, arr2), where arr1 and arr2 are the original sorted arrays.
+
+// You don't need to worry about validation, since arr1 and arr2 must be arrays with 0 or more Integers. 
+// If both arr1 and arr2 are empty, then just return an empty array.
+
+// Note: arr1 and arr2 may be sorted in different orders. Also arr1 and arr2 may have same integers. Remove duplicated in the returned result.
+
+// Examples (input -> output)
+// * [1, 2, 3, 4, 5], [6, 7, 8, 9, 10] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+function mergeArrays(arr1, arr2) {
+  let newArr = []
+  for(let i=0; i < arr1.length; i++) {
+    if(newArr.indexOf(arr1[i]) === -1) {
+      newArr.push(arr1[i])
+    }
+  }
+  for(let i=0; i < arr2.length; i++){
+    if(newArr.indexOf(arr2[i]) === -1) {
+      newArr.push(arr2[i])
+    }
+  }
+  return newArr.sort((a,b) => a-b)
+}
